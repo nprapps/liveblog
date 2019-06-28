d1d7124eac381b7e0b34c3567f7eff9855012ae5
Thomas Wilburn <twilburn@npr.org>
2019-06-27 21:52:49 -0400

    Revert code that was too clever for our own good.

--- www/js/app.js
+++ www/js/app.js
@@ -354,7 +354,7 @@ const deepLinkScroll = function() {
         // Delay scrolling to the post, to allow the header/pinned-post
         // Pym embed to load and update its height
         setTimeout(() => {
-            post.scrollIntoView();
+            scrollToPost('#'+postId);
             lazyload_assets(post);
         }, 500);
     }
